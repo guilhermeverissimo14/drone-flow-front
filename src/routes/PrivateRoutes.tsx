bf14f76166a11dd6be1c07570db61b4e64f53c5a
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-08-29 20:29:53 -0300

    feat: inicializando a configuração das rotas

---
+++ src/routes/PrivateRoutes.tsx
@@ -0,0 +1,50 @@
+
+import SignIn from "@/pages/auth/LoginForm";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+	// const { isAuthenticated } = useAuth();
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* Rotas públicas */}
+				<Route
+					path="/login"
+					// element={isAuthenticated ? <Navigate to="/" replace /> : <Login />}
+					element={<SignIn />}
+				/>
+				{/* <Route
+					path="/forgot-password"
+					element={
+						isAuthenticated ? <Navigate to="/" replace /> : <ForgotPassword />
+					}
+				/> */}
+
+				{/* Rotas privadas */}
+				{/* <Route
+					path="/"
+					element={
+						<PrivateRoute>
+							<Dashboard />
+						</PrivateRoute>
+					}
+				>
+					<Route index element={<Home />} />
+					<Route path="users" element={<Users />} />
+					<Route path="partners" element={<Prospectors />} />
+					<Route path="enterprises" element={<Enterprises />} />
+					<Route path="/enterprises/new" element={<EnterpriseForm />} />
+					<Route path="/enterprises/edit/:id" element={<EnterpriseForm />} />
+					<Route path="/plans" element={<Plans />} />
+					<Route path="reports" element={<Reports />} />
+				</Route> */}
+
+				{/* Rota de fallback */}
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default AppRoutes;
