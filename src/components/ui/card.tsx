77e8b1e3d899bdf0d7bd92738aaa5842cd645924
guilhermeverissimo14 <guilhermeverissimo14@gmail.com>
2025-09-03 11:30:04 -0300

    feat: add component card

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-title"
+      className={cn("leading-none font-semibold", className)}
+      {...props}
+    />
+  )
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-description"
+      className={cn("text-muted-foreground text-sm", className)}
+      {...props}
+    />
+  )
+}
+
+export { CardTitle, CardDescription }
